d311f3b91b72fd59fe05f3bf3ff564a286befe79
Jasonn5 <98439093+Jasonn5@users.noreply.github.com>
2022-06-17 19:25:06 -0400

    Add files via upload

---
+++ Services/CustomizeService.cs
@@ -0,0 +1,51 @@
+﻿using DataAccess.Repositories.Interfaces;
+using Entities;
+using Services.Helpers;
+using Services.Interfaces;
+using System.Collections.Generic;
+
+namespace Services
+{
+    public class CustomizeService : ICustomizeService
+    {
+        private readonly ICustomizeRepository _customizeRepository;
+
+        public CustomizeService(ICustomizeRepository customizeRepository)
+        {
+            _customizeRepository = customizeRepository;
+        }
+
+
+        public Customize FindActualCustom()
+        {
+            return _customizeRepository.FindActualCustom();
+        }
+
+        public ICollection<Customize> List()
+        {
+            var customizes = _customizeRepository.List();
+
+            return customizes;
+        }
+
+        public void Update(Customize customize)
+        {
+            var result = _customizeRepository.FindById(customize.Id);
+
+            if (result != null)
+            {
+                _customizeRepository.Update(customize);
+                var newCustomize = new Customize()
+                {
+                    DateOfCustom = TimeZoneHelper.GetSaWesternStandardTime(),
+                    Minimum = customize.Minimum,
+                    Maximum = customize.Maximum,
+                    Reason = customize.Reason,
+                    IsEnabled = true
+                };
+
+                _customizeRepository.Add(newCustomize);
+            }
+        }
+    }
+}
---
+++ Services/Interfaces/ICustomizeService.cs
@@ -0,0 +1,14 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Interfaces
+{
+    public interface ICustomizeService
+    {
+        void Update(Customize customize);
+        Customize FindActualCustom();
+        ICollection<Customize> List();
+    }
+}
